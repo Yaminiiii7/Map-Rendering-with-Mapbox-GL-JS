17c633c02246f54ca5181cf1c4dac2bcb05267bb
Yaminiiii7 <yaminiofficial789281@gmail.com>
2025-12-06 13:16:43 -0800

    Customised map and map Matching API implementation

---
+++ customisedMap/main.js
@@ -0,0 +1,10 @@
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieWFtaW5pLW1hbmRhZGkiLCJhIjoiY21pcnplMjEwMHh4MjNkcHdhN3l5amZnaCJ9.jribJH1wYoyGOri34V21pg';
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/yamini-mandadi/cmis0okeh000301subf7r4wny', // style URL
+  center: [-117.273618, 34.115515], // starting position [lng, lat]
+  zoom: 11.5 // starting zoom
+});
+
